a005df384836c7a792324ca5d9448cae8cb72195
GenesisDarklord <genesisdarklord@gmail.com>
2023-05-28 00:11:53 +0100

    Main Commit

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=60)
+    ci = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.nombre
